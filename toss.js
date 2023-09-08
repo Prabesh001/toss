8a0759d0d1772267ba63bad989a868904e692a5a
Prabesh001 <142589499+Prabesh001@users.noreply.github.com>
2023-09-08 21:51:16 +0545

    Add files via upload
    Toss a coin

---
+++ toss.js
@@ -0,0 +1,60 @@
+let result=document.querySelector('.result');
+
+function showResult(message){
+  setTimeout(()=>{
+    result.innerHTML=message;
+    result.style.display='block';
+  },2000)
+}
+
+function hideResult(){
+  result.style.display='none';
+}
+
+function toss1(){
+  const coinToss = document.querySelector(".coin");
+  coinToss.classList.remove('tossed');
+
+  hideResult();
+  
+  setTimeout(()=>{
+    coinToss.classList.add('tossed');
+    let randomNumber=Math.random();
+
+
+  if(randomNumber>=0 && randomNumber<1/2){
+    coinToss.innerHTML=`<img src="coinHead.jpg">`;
+
+    showResult('You Win!');
+
+  } else{
+    coinToss.innerHTML= `<img src="coinTail.jpg">`;
+
+    showResult('You Lose!');
+  }  
+},10);
+}
+
+function toss2(){
+  const coinToss = document.querySelector(".coin");
+
+  coinToss.classList.remove('tossed');
+
+  hideResult();
+  
+  setTimeout(()=>{
+    coinToss.classList.add('tossed');
+    let randomNumber=Math.random();
+
+  if(randomNumber>=0 && randomNumber<1/2){
+    coinToss.innerHTML=`<img src="coinHead.jpg">`;
+
+    showResult('You Lose!');
+
+  } else{
+    coinToss.innerHTML= `<img src="coinTail.jpg">`;
+
+    showResult('You Win!');
+  }  
+},10);
+}
